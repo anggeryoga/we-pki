ce4bb94b43deaf8c4e6eaedee169e3da9d7d7830
anggeryoga <139601862+anggeryoga@users.noreply.github.com>
2025-08-03 06:48:47 +0700

    Updated supabase.js

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
+const supabaseUrl = 'https://vyhwfepblcwczxiupyvz.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5aHdmZXBibGN3Y3p4aXVweXZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxNTQxODUsImV4cCI6MjA2OTczMDE4NX0.GZdJMfZLzZrkPaz_M-EvC5RFrkZqs9vIIkRW17Xtpdw';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
